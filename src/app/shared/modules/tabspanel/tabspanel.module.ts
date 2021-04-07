48f3fbd35fb2bbe9eddc5f0de0272c9ff0f38d5d
Shruthi <shruthisrao@gmail.com>
2021-04-07 16:07:57 +0530

    routerlink added

--- src/app/shared/modules/tabspanel/tabspanel.module.ts
+++ src/app/shared/modules/tabspanel/tabspanel.module.ts
@@ -5,8 +5,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TabspanelComponent } from './tabspanel.component';
 import { NgxSpinnerModule } from "ngx-spinner";
+ import { Routes, RouterModule } from '@angular/router';
 @NgModule({
-    imports: [CommonModule, NgbModule,  FormsModule, ReactiveFormsModule, NgxSpinnerModule],
+    imports: [CommonModule, NgbModule,  FormsModule, ReactiveFormsModule, NgxSpinnerModule, RouterModule.forRoot([])],
     declarations: [TabspanelComponent],
     exports: [TabspanelComponent]
 })
