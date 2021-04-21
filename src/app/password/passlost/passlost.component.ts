fcb68d0ef96464d651db4de150622753fcb01b6b
Shruthi <shruthisrao@gmail.com>
2021-04-21 15:49:32 +0530

    forgot pwd

--- src/app/password/passlost/passlost.component.ts
+++ src/app/password/passlost/passlost.component.ts
@@ -11,7 +11,7 @@ import { DBService } from '../../dbservices/db.service';
 })
 export class PasslostComponent implements OnInit, OnDestroy {
 
-  userName: string ; 
+  userEmail: string ; 
   result: any;
   ngUnsubscribe: Subject<any> = new Subject();
 
@@ -20,19 +20,23 @@ export class PasslostComponent implements OnInit, OnDestroy {
   ngOnInit() {
   
   this.result = 0;
-  this.userName = "";
+  this.userEmail = "";
   
   }
   resetpass()
   { 
 
   var hasMatch = false;
-  var iemail = { "email": this.userName};
+  var iemail = { "email": this.userEmail};
+  console.log(this.userEmail);
   this.dbService.getDatabyParam("users", iemail).subscribe(userData => setTimeout(() => {
-    if(userData["length"] > 0)
+  console.log(userData);
+  console.log(userData["body"]["length"]);
+    if(userData["body"]["length"]>0)
     {
-      if (userData[0].email == this.userName) {
-        //console.log("email exists" + userData[0].email);
+    
+      if (userData["body"][0].email == this.userEmail) {
+        console.log("email exists" + userData["body"][0].email);
           
           hasMatch = true;
       } 
@@ -40,11 +44,35 @@ export class PasslostComponent implements OnInit, OnDestroy {
    // console.log("hasmatched" + hasMatch);
     if(hasMatch == true) {
       this.result = 1; 
-     // this.helpService.SendEmailPasswordReset(this.userName,null);
+     this.SendEmailPasswordReset(this.userEmail,null);
     } else {
       this.result = 2;
     }
   }));
+  }
+
+
+  SendEmailPasswordReset(email,data) {
+    var paramstoken = { "email": email };
+    this.dbService.postData("users/resettoken", paramstoken).subscribe(emailData => setTimeout(() => {      
+      if (emailData) {      
+        // var apiUrl = window.location.origin;
+        var resetLink = window.location.origin + "/resetpassword;token=" + encodeURIComponent(emailData["hash"]) + ";email=" + email;
+        data['resetLink'] = resetLink;
+        data['email'] = email;
+        var IemailSubject = this.FormatEmailContent(data.emailSubject, data);
+        var IemailContent = this.FormatEmailContent(data.emailContent, data); 
+        let Emaildata: any = { "to": email,  "from": environment.fromname+environment.fromemail, "datetime": new Date(), "subject": IemailSubject, "content": IemailContent, "contenthtml": IemailContent };
+        // console.log(Emaildata);
+        // send email     
+        this.SaveEmailData(null, Emaildata);    
+        this.dbService.postData("email", Emaildata).subscribe(emailData => setTimeout(() => {
+          if (emailData) {
+          }
+          return emailData;
+        }, 0));       
+      }
+    }));
   }
 ngOnDestroy() {
     this.ngUnsubscribe.next();
