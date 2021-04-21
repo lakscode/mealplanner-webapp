4b8e161cd8cf8d0701fd86ac6dc022fff2a96aff
Shruthi <shruthisrao@gmail.com>
2021-04-21 22:04:30 +0530

    plancreate

--- src/app/planner/plannercreate/plannercreate.component.ts
+++ src/app/planner/plannercreate/plannercreate.component.ts
@@ -42,6 +42,7 @@ export class PlannercreateComponent implements OnInit {
 	pageNosList:  Array<any> = [];
 	updatingFlag : boolean = false;
 	pageCount: any = 5;
+	errorMessage: any = "";
 	constructor(private router: Router, private route: ActivatedRoute, private pdfService: PDFService, private userService: UserService, private dbService: DBService, private helpService: HelpService, private formBuilder: FormBuilder) {
 	
 	}
@@ -765,7 +766,9 @@ this.plan["days"][r]["meals"][c]["recipe"] =  this.formatRecipe(recipeItem);
   }
  
   saveMealPlan()
-  {
+  { 
+  this.errorMessage ="";
+    if(this.plan["name"] !== ""){
 	if(!this.updatingFlag )
 	{
 		this.updatingFlag = true;
@@ -817,6 +820,9 @@ console.log(params);
 			}
 		}));
 		}
+	}
+	} else {
+	this.errorMessage = "Plan Name is required";
 	}
 
   }
