9bd6b72e371db7d612226b7870968385253aa21f
Shruthi <shruthisrao@gmail.com>
2021-05-05 21:54:32 +0530

    minor changes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -62,6 +62,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 					{"id":"display", "menu":"FAQs", "link":"/faqs", "display":displayMenu, "active":false},
 				//	{"id":"blogs", "menu":"Blogs", "link":"/blogs", "display":displayMenu, "active":false},
 				//	{"id":"testimonials", "menu":"Testimonials", "link":"/testimonials", "display":displayMenu, "active":false}
+				{"id":"questionnaire", "menu":"Questionnaire", "link":"/questionnaire", "display":displayMenu, "active":false}
 				]
 			},
 			{"id":"features", "menu":"Features", "link":"/features", "display":displayMenu, "active":false},
@@ -80,7 +81,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 					{"id":"display", "menu":"FAQs", "link":"/faqs", "display":displayMenu, "active":false},
 				//	{"id":"blogs", "menu":"Blogs", "link":"/blogs", "display":displayMenu, "active":false},
 				//	{"id":"testimonials", "menu":"Testimonials", "link":"/testimonials", "display":displayMenu, "active":false}
-					{"id":"questionnaire", "menu":"Questionnaire", "link":"/questionnaire", "display":displayMenu, "active":false}
+					//{"id":"questionnaire", "menu":"Questionnaire", "link":"/questionnaire", "display":displayMenu, "active":false}
 				]
 			},
 			{"id":"recipes", "menu":"Recipes", "link":"/recipes", "display":displayMenu, "active":false,
