01055fa225da2eebeaa3f2b7fd234b2d68ae769c
Shruthi <shruthisrao@gmail.com>
2021-05-07 16:35:21 +0530

    removed unused and commented - faq

--- src/app/faqs/faqs.component.ts
+++ src/app/faqs/faqs.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit,OnDestroy  } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
-import { UserService } from '../services/user.service';
-import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { DBService } from '../dbservices/db.service';
-import { HelpService } from '../services/help.service';
-
-import { environment } from './../../environments/environment';
-declare var $: any;
-
+import { Router } from "@angular/router";
 
 @Component({
 	selector: 'app-faqs',
@@ -16,7 +8,7 @@ declare var $: any;
 })
 export class FaqsComponent implements OnInit {
 	stepsList: Array<any> = [];
-	constructor(private router: Router, private route: ActivatedRoute, private userService: UserService, private dbService: DBService, private helpService: HelpService, private formBuilder: FormBuilder) {
+	constructor(private router: Router) {
 	
 	}
 
@@ -24,13 +16,13 @@ export class FaqsComponent implements OnInit {
 
 	this.stepsList = [];
 
-		this.stepsList.push({"label":"Features", "subtitle":"Question 1", "image":"assets/images/temp-images/main-recipe3.jpg", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam temporee."});
+		this.stepsList.push({"subtitle":"Question 1", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam temporee."});
 
-		this.stepsList.push({"label":"Choose", "subtitle":"Question 2","image":"assets/images/temp-images/new-var2.jpg", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore"});
+		this.stepsList.push({"subtitle":"Question 2", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore"});
 
-		this.stepsList.push({"label":"Design", "subtitle":"Question 3","image":"assets/images/temp-images/steps-tab1.jpg", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore"});
+		this.stepsList.push({ "subtitle":"Question 3", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore"});
 
-		this.stepsList.push({"label":"Design", "subtitle":"Question 4","image":"assets/images/temp-images/Meal-Planning.jpg", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore"});
+		this.stepsList.push({"subtitle":"Question 4", "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore"});
 
 
 
--- src/app/faqs/faqs.module.ts
+++ src/app/faqs/faqs.module.ts
@@ -3,16 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { FaqsRoutingModule } from './faqs-routing.module';
 import { FaqsComponent } from './faqs.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import {SidebarModule } from "../shared/modules";
 @NgModule({
   imports: [
     CommonModule,
     FaqsRoutingModule,
-    FlexLayoutModule,
-	FormsModule,
-	ReactiveFormsModule,
 	SidebarModule
   ],
   declarations: [FaqsComponent]
