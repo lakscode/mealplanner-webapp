9547fc15b05b0b1dbf4f8e7d8946e46df15eb44e
Lakshmi <writetolaks@gmail.com>
2021-05-15 13:00:04 +0530

    recipe book:

--- src/app/recipes/communities/communities.component.ts
+++ src/app/recipes/communities/communities.component.ts
@@ -236,7 +236,7 @@ endIndex = startIndex+ endIndex;
 
 
 	gotoRecipebook(id){
-	this.router.navigate(['recipebook', id]);
+	this.router.navigate(['community', id]);
 	}
 
  gotopage(page , params = null)
