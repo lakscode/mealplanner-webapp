5434eae9c154932ac24b8d26ea56b12d2666e414
Shruthi <shruthisrao@gmail.com>
2021-06-01 18:11:58 +0530

    Update header.component.ts

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -113,7 +113,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 				[
 
 					{"id":"planner-plan-list", "menu":"Meal Plans", "link":"/plans", "display":displayMenu, "active":false},
-				//	{"id":"planner-plan-create", "menu":"Create Plan", "link":this.planLink, "display":displayMenu, "active":false}
+					{"id":"planner-plan-create", "menu":"Create Plan", "link":this.planLink, "display":displayMenu, "active":false}
 				]
 			}	,
 			{"id":"communities", "menu":"Communities", "link":"/communities", "display":displayMenu, "active":false,
