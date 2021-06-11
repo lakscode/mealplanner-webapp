216aa2d4c64970aff7c6d1c54ae5e38be967d6e9
Lakshmi <writetolaks@gmail.com>
2021-06-11 15:10:51 +0530

    save url which couldnt be imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -105,11 +105,7 @@ import {environment} from "../environments/environment";
     BenefitsModule,
 	LandingModule,
 	ToastrModule.forRoot({
-<<<<<<< HEAD
-		timeOut:3000,
-=======
 		timeOut:5000,
->>>>>>> c0ac0d0bfaab41606986d6367e8f125b97c2f1ad
 		positionClass: 'toast-top-full-width',
 		preventDuplicates: true,
 		closeButton: true
