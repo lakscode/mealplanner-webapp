0f820aa81d11496065b2ee77b2891f4430e55284
Lakshmi <writetolaks@gmail.com>
2021-06-21 22:00:31 +0530

    details page fiber and cholesterol

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -542,7 +542,7 @@ transform(value: any) {
 	 // var params = {"limit": 100};
 	 // params["createdby"] = this.currentUser["id"];
 	  console.log(params);
-	  var params = {"query": "SELECT c.*, COUNT(rm.id) AS recipecount, u.email, u.firstname, u.lastname FROM collection AS c LEFT JOIN recipe_mapping AS rm ON c.id = rm.collection_id LEFT JOIN users AS u ON c.created_by = u.id GROUP BY c.id limit 0, 4"};
+	  var params = {"query": "SELECT c.*, COUNT(rm.id) AS recipecount, u.email, u.firstname, u.lastname FROM collection AS c LEFT JOIN recipe_mapping AS rm ON c.id = rm.collection_id LEFT JOIN users AS u ON c.created_by = u.id GROUP BY c.id limit 1, 4"};
 
 	  var res =   this.dbService.getDatabyTablebyQuery("collection", params).subscribe(invData => setTimeout(() => {
   
