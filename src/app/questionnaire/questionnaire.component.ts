1169e5f527f4b5b5b01d0b540fd600906e663748
Lakshmi <writetolaks@gmail.com>
2021-06-27 18:59:01 +0530

    questionnaire

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -73,7 +73,7 @@ export class  QuestionnaireComponent implements OnInit {
     }
 	this.loadQuestionnaire();
   }
-
+  questionnaire: any = [];
   loadQuestionnaire()
   {
 	var params= {};
@@ -83,62 +83,140 @@ export class  QuestionnaireComponent implements OnInit {
   	}  
 	var res =   this.dbService.getDataByTable("questionnaire", params).subscribe(invData => setTimeout(() => {
        console.log(invData);
+	   if(invData["body"] && invData["body"]["length"] > 0)
+	   {
+		this.questionnaire  = [];
+		this.questionnaire.push(invData["body"][0]);
+		this.params["id"] = invData["body"][0]["id"];
+		this.loadData();
+
+	   }
 	}));
 
+  }
+
+  loadData()
+  {
+	  console.log(this.questionnaire);
+	for(let o=0; o <  this.questions["length"]; o++)
+	{
+		if(this.questionnaire[0]["question"+(o+1)] !== "")
+		{
+			this.questions[o]["answer"] = this.questionnaire[0]["question"+(o+1)];
+		}
+		for(let p=0; p <  this.questions[o]['answers']["length"]; p++)
+		{
+			if(this.questions[o]["type"] == "option")
+			{
+				if(this.questions[o]['answers'][p]["text"] == this.questionnaire[0]["question"+(o+1)])
+				{
+					this.questions[o]['answers'][p]["selected"]  = true;
+				}
+			}
+
+			if(this.questions[o]["type"] == "checkbox")
+			{
+				if(this.questionnaire[0]["question"+(o+1)].indexOf(this.questions[o]['answers'][p]["text"]) !== -1 )
+				{
+					this.questions[o]['answers'][p]["selected"]  = true;
+				}
+			}
+
+		}
+		console.log(this.questions[o]);
+	}
   }
   complete()
   {
 
+	console.log(this.params);
     this.processing = true;
     console.log("complete");
     console.log(this.params);
+	delete this.params["tablename"];
+
     var uniqueid =  localStorage.getItem("uniqueid");
 
-    var params = {};
-    params = this.params;
+    var paramsr = {};
+	if(typeof(this.params["id"]) !== "undefined" &&this.params["id"] !== null && this.params["id"] !== "")
+    {
+      paramsr["id"] = this.params["id"];
+    }
+	for(let p = 1; p <10; p++)
+	{
+		if(typeof(this.params["question" + p]) !== "undefined" && this.params["question" + p] !== "")
+		{
+			console.log("p " + p);
+			console.log(this.params["question"]  + p.toString());
+			paramsr["question" + p.toString()] = this.params["question" + p.toString()];
+		}
+	}
     //uniqueid = "1a15067e-ab63-a2ed-3582-220714793548";
     this.uniqueid = uniqueid;
-    if(typeof(uniqueid) !== "undefined" && uniqueid !== null && uniqueid !== "")
+    if(this.params["user_uniqueid"] == "" || (typeof(uniqueid) !== "undefined" && uniqueid !== null && uniqueid !== ""))
     {
-      params["user_uniqueid"] = uniqueid;
+      paramsr["user_uniqueid"] = uniqueid;
     }
     else
     {
       var uniqueid = this.helpService.GenerateUniqueId(20);
-      params["user_uniqueid"] = uniqueid;
+      paramsr["user_uniqueid"] = uniqueid;
       localStorage.setItem("uniqueid",uniqueid);
     }
 
     var ipaddress =  localStorage.getItem("ipaddress");
-    if(typeof(ipaddress) !== "undefined" && ipaddress !== null && ipaddress !== "")
+    if(this.params["user_ipaddress"] == "" && typeof(ipaddress) !== "undefined" && ipaddress !== null && ipaddress !== "")
     {
-      params["user_ipaddress"] = ipaddress;
+      paramsr["user_ipaddress"] = ipaddress;
     }
-    if(this.currentUser !== null && typeof(this.currentUser["id"]) !=="undefined")
+    if(this.params["userid"] == "" && this.currentUser !== null && typeof(this.currentUser["id"]) !=="undefined")
 		{
-      params["userid"] = this.currentUser["id"];
+      paramsr["userid"] = this.currentUser["id"];
     }
-    if((typeof(uniqueid) !== "undefined" && uniqueid !== null && uniqueid !== "") || (typeof(ipaddress) !== "undefined" && ipaddress !== null && ipaddress !== ""))
+    if(typeof(this.currentUser["id"]) !== "undefined" || this.currentUser["id"] !== "" || (typeof(uniqueid) !== "undefined" && uniqueid !== null && uniqueid !== "") || (typeof(ipaddress) !== "undefined" && ipaddress !== null && ipaddress !== ""))
     {
     
-     
-    //  params["user_uniqueid"] = uniqueid;
-      var res =   this.dbService.postDataByTable("questionnaire", params).subscribe(invData => setTimeout(() => {
-        this.processing = false;
-
-        localStorage.setItem("q_complete","true");
-        localStorage.setItem("questions", JSON.stringify(params));
-
-		if( this.currentUser["id"] !== "")
+		console.log(paramsr);
+		if(typeof(paramsr["id"] ) !== "undefined" && paramsr["id"] !== "")
 		{
-			this.router.navigate(["landing"]);
+			console.log("updaitn gquesitons");
+			var res =   this.dbService.updateDataByTable("questionnaire", paramsr).subscribe(invData => setTimeout(() => {
+				this.processing = false;
+		
+				localStorage.setItem("q_complete","true");
+				localStorage.setItem("questions", JSON.stringify(paramsr));
+		
+				if( this.currentUser["id"] !== "")
+				{
+					this.router.navigate(["landing"]);
+				}
+				else
+				{
+					this.router.navigate(["pricing"]);
+				}
+		
+			  }));
 		}
 		else
 		{
-        	this.router.navigate(["pricing"]);
-		}
+			console.log("addign gquesitons");
+			var res =   this.dbService.postDataByTable("questionnaire", paramsr).subscribe(invData => setTimeout(() => {
+				this.processing = false;
 
-      }));
+				localStorage.setItem("q_complete","true");
+				localStorage.setItem("questions", JSON.stringify(paramsr));
+
+				if( this.currentUser["id"] !== "")
+				{
+					this.router.navigate(["landing"]);
+				}
+				else
+				{
+					this.router.navigate(["pricing"]);
+				}
+
+			}));
+		}
 
     }
   }
@@ -184,23 +262,25 @@ export class  QuestionnaireComponent implements OnInit {
 			}
 		  }
 
-		  if(this.questions[cq]["answer"].indexOf(answer['text']) == -1)
-		  {
-			  if(this.questions[cq]["answer"] !== "")
-			this.questions[cq]["answer"] = this.questions[cq]["answer"] + "," + answer['text'];
-			else
-			this.questions[cq]["answer"] = answer['text'];
+		  this.questions[cq]["answer"] = answer['text'];
 
-			this.params["question" + (cq+1)] = this.questions[cq]["answer"];
-		  }
+		  this.params["question" + (cq+1)] = answer['text'];
 	  } 
 
    if(type == "checkbox")
    {
    		answer["selected"] = !answer["selected"];
-    	this.questions[cq]["answer"] = answer['text'];
+    	
+		if(this.questions[cq]["answer"].indexOf(answer['text']) == -1)
+			{
+				if(this.questions[cq]["answer"] !== "")
+			  this.questions[cq]["answer"] = this.questions[cq]["answer"] + "," + answer['text'];
+			  else
+			  this.questions[cq]["answer"] = answer['text'];
+  
+			  this.params["question" + (cq+1)] = this.questions[cq]["answer"];
+			}
 
-    	this.params["question" + (cq+1)] = answer['text'];
    }
    
   }
